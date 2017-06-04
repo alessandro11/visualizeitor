7efcb59d2efb45ad7e03642f1340d09a53fecb54
Alessandro Elias <ae11@c3sl.ufpr.br>
2017-06-04 15:19:08 -0300

    Add many functionality;.
    Signed-off-by: Alessandro Elias <ae11@c3sl.ufpr.br>

--- xmlparser.js
+++ xmlparser.js
@@ -28,29 +28,80 @@ function GetXMLDoc(xml) {
 	*/
 }
 
+var disc_undef = [];
+function parse_aluno(aluno) {
+	var cod_disc = aluno.childNodes[29].firstChild.nodeValue;
+	var situacao;
+
+	if( aluno.childNodes[53].firstChild === null )
+		return;
+
+	situacao = aluno.childNodes[53].firstChild.nodeValue;
+	/*
+	 * Fix name, in case name does follow valid syntaxe.
+	 */
+	if( situacao === "Repr. Freq" ) situacao = "Reprovado";
+	if( situacao === "Disp. c/nt" ) situacao = "Dispensa";
+	if( situacao === "Tr. Total") situacao = "Tr_total";
+	disc_td = document.getElementById(cod_disc);
+	if( disc_td === null ) {
+		disc_undef.push(cod_disc);
+	} else {
+		disc_td.classList.toggle(situacao, true);
+	}
+}
+
+function remove_class_situation() {
+	var lst_td = document.getElementsByTagName("td");
+
+	for( i = 0; i < lst_td.length; ++i ) {
+		a = lst_td[i];
+		lst_td[i].classList.toggle("Aprovado", false);
+		lst_td[i].classList.toggle("Reprovado", false);
+		lst_td[i].classList.toggle("Equivale", false);
+	}
+}
+
 function btn_Click_Search() {
 	if( xmldoc === undefined ) {
 		alert("Erro, xmldoc!");
 		return;
 	}
 
+	remove_class_situation();
+
 	var grr = txt_grr.value;
 	var alunos = xmldoc.getElementsByTagName("ALUNO");
 	var aluno;
 	var cod_dic;
+	var count = 0;
 
 	for( i = 0; i < alunos.length; ++i ) {
 		aluno = alunos[i];
 		if( aluno.childNodes[3].firstChild.nodeValue === grr ) {
-			cod_dic = alunos[i].nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.firstChild.nodeValue;
-			console.log(cod_dic);
+			parse_aluno(aluno);
+			count++;
 		}
 	}
+
+	if( count === 0 ) {
+		alert("Código: '" + grr + "' não encontrado!");
+		return;
+	}
+
+	if( disc_undef.length > 0 ) alert("Disciplina(s) não localizadas:\n" + disc_undef);
 }
 
+function txt_grr_KeyPress(event) {
+	var input = event.which || event.KeyCode;
+
+	if( input === 13 )
+		btn_Click_Search();
+}
 
 document.addEventListener("DOMContentLoaded", function(event) {
 	loadXMLDoc();
 	txt_grr = document.getElementById("txt_grr");
 });
 document.getElementById("btn_search").addEventListener("click", btn_Click_Search, true);
+document.getElementById("txt_grr").addEventListener("keypress", txt_grr_KeyPress, true);
